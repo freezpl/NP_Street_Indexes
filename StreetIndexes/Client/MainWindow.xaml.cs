018cb74eed44af77ed0e12edc97ccdb44392e5fe
freezpl <freezpl@gmail.com>
2019-04-02 08:20:20 +0300

    start migrate to mvvm

--- StreetIndexes/Client/MainWindow.xaml.cs
+++ StreetIndexes/Client/MainWindow.xaml.cs
@@ -28,34 +28,45 @@ namespace Client
     {
         public ObservableCollection<string> Places { get; set; }
         const string REQ_AUTOCOMP = @"https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyCRuMrU5eZqvCXaxRg8FOacV9iea6SBEkI&input=";
-        
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MVVM();
+
 
             Places = new ObservableCollection<string>();
             SearchBox.ItemsSource = Places;
+
+            //Task tt = new Task(() =>
+            //{
+            //    App.Current.Dispatcher.Invoke(() => { Wind.Title = "Hi!"; });
+            //    Thread.Sleep(1000);
+            //});
+            //tt.Start();
+            //tt.Wait();
+            
+            //MessageBox.Show(tt.Status.ToString());
             
         }
 
         Task t;
+        CancellationTokenSource cts;
 
-        void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        async void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             //if (SearchBox.Text.Length == 0)
             //{
             //    return;
             //}
-            if(SearchBox.IsEnabled)
-            {
-                SearchBox.IsEnabled = false;
-                string reqStr = REQ_AUTOCOMP + SearchBox.Text;
-                t = new Task(()=>Search(reqStr));
-                t.Start();
-                t.Wait();
-                SearchBox.IsEnabled = true;
-            }
-            
+
+            //string reqStr = REQ_AUTOCOMP + SearchBox.Text;
+            //t = new Task(()=>Search(reqStr));
+            //t.Start();
+            //t.Wait();
+            //cts = new CancellationTokenSource();
+            //await Task.Run(() => Search(reqStr), cts.Token);
+
             //using (StreamReader reader = new StreamReader(response.GetResponseStream()))
             //{
             //    string responseText = reader.ReadToEnd();
@@ -78,8 +89,14 @@ namespace Client
         int counter = 0;
         void Search(string req)
         {
+            if (cts.Token.IsCancellationRequested)
+            {
+                return;
+            }
             counter++;
-            Dispatcher.Invoke(()=>{ Title = "Hi!"; });
+            Thread.Sleep(1000);
+            App.Current.Dispatcher.Invoke(() => { Wind.Title = counter.ToString(); });
+
             //MessageBox.Show("search");
             //WebRequest request = WebRequest.Create(req);
             //WebResponse response = request.GetResponse();
---
+++ StreetIndexes/Client/Models/MVVM.cs
@@ -0,0 +1,52 @@
+﻿using Client.Helper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Client.Models
+{
+    public class MVVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        //commands
+        AppCommand typeCmd;
+        public AppCommand TypeCmd
+        {
+            get
+            {
+                return typeCmd ?? (typeCmd = new AppCommand((o) =>
+                {
+                    MessageBox.Show("ccc");
+                }));
+            }
+        }
+
+
+        private string searchText;
+        
+        public string SearchText
+        {
+            get
+            {
+                return searchText;
+            }
+            set
+            {
+                searchText = value;
+                MessageBox.Show(searchText);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SearchText)));
+            }
+        }
+
+        public MVVM()
+        {
+
+        }
+
+    }
+}
